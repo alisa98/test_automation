93af20e9a07c19d83ab5aa5ee48dadf47047a548
ALisa Utlik <alisa.utlik@gmail.com>
2018-10-14 16:07:03 +0300

    method IsTriangle with UnitTests

---
+++ UnitTest/tests/UnitTest1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        [TestMethod]
+        public void IsTrueIfTheDataValid()
+        {
+                Assert.AreEqual(Triangle.IsTriangle(3,4,5));
+        }
+
+        [TestMethod]
+        public void IsFalseIfTheDataNotValid()
+        {
+            Assert.AreNotEqual(Triangle.IsTriangle(1,2,3));
+
+        }
+
+        [TestMethod]
+        public void IsFalseIfTheSideIsNegative()
+        {
+            Assert.AreNotEqual(Triangle.IsTriangle(3, 4, -5 ));
+
+        }
+
+        [TestMethod]
+        public void IsFalseIfTwoSideAreNegative()
+        {
+            Assert.AreNotEqual(Triangle.IsTriangle(3, -4, -5));
+
+        }
+
+
+        [TestMethod]
+        public void IsTrueIfTheSidesAreEquals()
+        {
+            Assert.AreEqual(Triangle.IsTriangle(4, 4, 6));
+        }
+
+        [TestMethod]
+        public void IsTrueIfSidesAreEquals()
+        {
+            Assert.AreEqual(Triangle.IsTriangle(4, 4, 4));
+        }
+
+        [TestMethod]
+        public void IsFalseIfTtheSideIsZero()
+        {
+            Assert.AreNotEqual(Triangle.IsTriangle(3, 4, 0));
+        }
+
+        [TestMethod]
+        public void IsFalseIfTwoSidesAreZero()
+        {
+            Assert.AreNotEqual(Triangle.IsTriangle(3, 0, 0));
+        }
+
+
+        [TestMethod]
+        public void IsFalseIfTtheSidesAreZero()
+        {
+            Assert.AreNotEqual(Triangle.IsTriangle(0, 0, 0));
+        }
+      
+
+
+        [TestMethod]
+        public void IsFalseIfTwoSidesEqualsToTheThird()
+        {
+            Assert.AreNotEqual(Triangle.IsTriangle(3, 7, 10));
+        }
+
+
+
+    }
+}
---
+++ UnitTest/triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public static class Triangle
+    {
+       
+
+           public  static bool IsTriangle(double a, double b, double c)
+            {
+              
+            
+                if (a <= 0 || b <= 0 || c <= 0)
+                    return false;
+            
+                return a + b > c && a + c > b && b + c > a;
+                
+            }
+       
+        
+    }
+}
