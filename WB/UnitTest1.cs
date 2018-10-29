56172a512cc52aae05f83b2f08cdf298432ee119
ALisa Utlik <alisa.utlik@gmail.com>
2018-10-29 23:09:40 +0300

    add WB

---
+++ WB/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AirlinesTestingApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.turkishairlines.com/");
+            Thread.Sleep(1000);
+
+            IWebElement element = driver.FindElement(By.XPath("//input[@type='text'])[2]"));           
+            element.SendKeys("Москва");
+
+
+            Thread.Sleep(1000);
+
+            IWebElement buttun_OK = driver.FindElement(By.Id("executeSingleCitySubmit_i"));
+            buttun_OK.Click();
+
+            String title = driver.Title;
+            Assert.AreEqual(true, title.Contains(expectedTitle), "The information you have entered is incomplete.");
+
+
+
+
+        }
+    }
+}
