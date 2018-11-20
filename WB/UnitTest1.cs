08c475bc3b2f1a77b7fcc210716e7c5b0cc522c1
autlik <alisa.utlik@gmail.com>
2018-11-20 16:49:12 +0300

    correcting code

--- WB/UnitTest1.cs
+++ WB/UnitTest1.cs
@@ -3,24 +3,18 @@ using Xunit;
 
 namespace XUnitTestProject2
 {
-    public class UnitTest1
+    public class Search
     {
         [Fact]
-        public void Test1()
+        public void SearchWillNotBeRealizedIfThePointOfArrivalIsNotEntered()
         {
             IWebDriver driver = new ChromeDriver();
-
             driver.Navigate().GoToUrl("https://www.turkishairlines.com/");
-
             IWebElement element = driver.FindElement(By.XPath("//input[@type='text'])[2]"));
             element.SendKeys("Москва");
-
             IWebElement buttun_OK = driver.FindElement(By.Id("executeSingleCitySubmit_i"));
             buttun_OK.Click();
-
-            String title = driver.Title;
-            Assert.True(title.Contains(expectedTitle), "The information you have entered is incomplete.");
-
+            Assert.True(driver.Title.Contains("The information you have entered is incomplete."));
         }
     }
 }
