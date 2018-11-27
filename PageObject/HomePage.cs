1b9d37d80d8a22b52c32be2b6b25e4d8a482e08e
autlik <alisa.utlik@gmail.com>
2018-11-27 13:02:11 +0300

    change file Test.cs

--- PageObject/HomePage.cs
+++ PageObject/HomePage.cs
@@ -3,76 +3,46 @@ using OpenQA.Selenium.Support.UI;
 using System;
 using System.Threading;
 
-namespace AirlinesTestingApp
+namespace AirlinesTestingApp.Pages
 {
     public class HomePage
     {
-        private  IWebDriver driver;
-
+        private IWebDriver driver;
         private const string url = "https://www.turkishairlines.com/";
 
-        [FindsBy(How = How.ClassName, Using = "input.input-block-level.form-control")]
-        public IWebElement departure { get; set; }
-
-        [FindsBy(How = How.ClassName, Using = ".open > .dropdown-menu .input-block-level")]
-        public IWebElement arrival { get; set; }
-
-        By buttonCalendar = By.ClassName(".active > .opt");
-        public IWebElement buttonCalendar { get; set; }
-
-        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'30')]")]
-        public IWebElement inpuDate { get; set; }
-
-        [FindsBy(How = How.XPath, Using = "(//a[contains(text(),'ОК')])[2]")]
-        public IWebElement buttonOK { get; set; }
-
-        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Поиск')]")]
-        public IWebElement buttonSearch { get; set; }
-
-        [FindsBy(How = How.XPath, Using = "//a[contains(text(),'Премиальный билет')])[2])]")]
-        public IWebElement awardTicket { get; set; }
+        By clickAddPassenger = By.CssSelector("span.cabin-view-type");
+        By plusBaby = By.Xpath("(//span[@name='upperCount'])[2]");
+        By minusAdult = By.CssSelector("span[name='lowerCount']");
+        By errorsMessages = By.ClassName("messages");
 
         public HomePage(IWebDriver driver)
         {
             this.driver = driver;
-
-
-            //if (!"Login".Equals(driver.getTitle()))
-            //{
-            //    throw new IllegalStateException("This is not the login page");
-            //}
         }
 
-        public HomePage GoToHomePage(IWebDriver driver)
+        public void OpenHomePage()
         {
             driver.Navigate().GoToUrl(url);
-            return new HomePage(driver);
-        }
-        public void SelectDeparure()
-        {
-            driver.FindElement(departure).Click();
-        }
-        public void SelectArrival()
-        {
-            driver.FindElement(arrival).Click();
-        }
-        public void SelectButtonCalendar()
-        {
-            driver.FindElement(buttonCalendar).Click();
-        }
-        
-        public void SelectButtonOK()
-        {
-            driver.FindElement(buttonOK).Click();
-        }
-        public void SelectSearch()
-        {
-            driver.FindElement(buttonSearch).Click();
         }
 
-        public void SelectAwardTicket()
+        public void ClkickAddPassengers()
         {
-            driver.FindElement(awardTicket).Click();
-        }     
+            driver.FindElement(clickAddPassenger).Click();
+        }
+
+        public void PlusBaby()
+        {
+            driver.FindElement(plusBaby).Click();
+        }
+
+        public void MinusAdult()
+        {
+            driver.FindElement(minusAdult).Click();
+        }
+
+        public IWebElement GetErrorsMessages()
+        {
+            return driver.FindElement(errorsMessages);
+        }
     }
 }
---
+++ PageObject/Test.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+namespace WebDriver
+{
+    [TestFixture()]
+    public class Test
+    {
+        private HomePage homePage;
+        private const string errorMessage = "The information you have entered is incomplete.";
+
+        [Test()]
+
+        public void OneBabyOnOneAdult()
+        {
+            OpenHomePage();
+            AddBabyAndMinusAdult();
+            AssertErrorsVisible();
+        }
+
+        private void OpenHomePage()
+        {
+            var homePage = new HomePage(new ChromeDriver());
+            homePage.OpenHomePage();
+            homePage.CloseAds();
+        }
+
+        private void AddBabyAndMinusAdult()
+        {
+            var homePage = new HomePage(new ChromeDriver());
+            homePage.OpenHomePage();
+            homePage.CloseAds();
+
+            homePage.AddPassengers();
+            homePage.PlusBaby();
+            homePage.MinusAdult();
+        }
+
+        private void AssertErrorsVisible()
+        {
+            var messageText = homePage.GetErrorsMessages().Text;
+            Assert.AreEqual(errorMessage, messageText);
+        }
+    }
+}
